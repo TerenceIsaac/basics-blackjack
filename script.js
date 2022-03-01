98477dbd9dd28e71d3bdb7afa78fd70d5c028435
TerenceIsaac <limyiloong@hotmail.com>
2022-03-02 00:03:59 +0800

    edited script and scriptAns with index.html

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// Initialise an empty deck array
+// Initialise an empty deck array & global var
 var cardDeck = [];
+var playerCard = [{}];
+var computerCard = [{}];
+var myOutputValue;
 
 var makeDeck = function () {
   // Initialise an array of the 4 suits in our deck. We will loop over this array.
@@ -78,4 +81,54 @@ var shuffleCards = function (cardDeck) {
   }
   // Return the shuffled deck
   return cardDeck;
+};
+
+var main = function () {
+  //make deck and shuffle
+  makeDeck();
+
+  //// make playerCards and computerCards (deal 2 cards each)
+  // var playerCard1 = cardDeck.pop();
+  // var computerCard1 = cardDeck.pop();
+  // var playerCard2 = cardDeck.pop();
+  // var computerCard2 = cardDeck.pop();
+
+  // playerCard.push(playerCard1);
+  // playerCard.push(playerCard2);
+  // computerCard.push(computerCard1);
+  // computerCard.push(computerCard2);
+
+  // console.log("playerCard");
+  // console.log(playerCard);
+  // console.log("computerCard");
+  // console.log(computerCard);
+
+  var index = 0;
+  var playerTotalRank = 0;
+  var computerTotalRank = 0;
+  while (index <= playerCard.length) {
+    playerTotalRank = playerTotalRank + playerCard[index].rank;
+    console.log("Player Total Rank");
+    console.log(playerTotalRank);
+    if (playerTotalRank < 16) {
+      playerCard[index] = cardDeck.pop();
+    }
+
+    computerTotalRank = computerTotalRank + computerCard[index].rank;
+    console.log("Computer Total Rank");
+    console.log(computerTotalRank);
+    if (computerTotalRank < 16) {
+      computerCard[index] = cardDeck.pop();
+    }
+
+    index = index + 1;
+  }
+};
+
+var game1 = function () {
+  if (playerCard1.rank + playerCard2 == 11) {
+    myOutputValue = `Player Wins`;
+  } else if (computerCard1.rank + computerCard2.rank == 11) {
+    myOutputValue = `Computer Wins`;
+  }
 };
