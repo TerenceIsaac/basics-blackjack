ba8d008de358adbef20b79198fffb5d9f93b02c9
TerenceIsaac <limyiloong@hotmail.com>
2022-03-05 23:23:32 +0800

    1st Finished Draft Script.js and Index HTML. First Playable Game

--- script.js
+++ script.js
@@ -1,134 +1,290 @@
-// Initialise an empty deck array & global var
-var cardDeck = [];
-var playerCard = [{}];
-var computerCard = [{}];
-var myOutputValue;
+// Initialise global var
+var playerCard = [];
+var computerCard = [];
+var GameOverOutputValue;
+var playerWinCount = 0;
+var computerWinCount = 0;
 
 var makeDeck = function () {
-  // Initialise an array of the 4 suits in our deck. We will loop over this array.
-  var suits = ["❤", "♦", "♣", "♠"];
+  // create the empty deck at the beginning
+  var carddeck = [];
+  var suits = ["❤", "🔷", "♣", "♠"];
 
-  // Loop over the suits array
   var suitIndex = 0;
   while (suitIndex < suits.length) {
-    // Store the current suit in a variable
+    // make a variable of the current suit
     var currentSuit = suits[suitIndex];
 
-    // Loop from 1 to 13 to create all cards for a given suit
-    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
-    // This is an example of a loop without an array.
-    var rankCounter = 1;
-    while (rankCounter <= 13) {
-      // By default, the card name is the same as rankCounter
+    // loop to create all cards in this suit
+    // rank 1-13
+    var counter = 1;
+    while (counter <= 13) {
+      var rankCounter = counter;
       var cardName = rankCounter;
 
-      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      // for BlackJack only, change the card rank for the face cards to 10.
       if (cardName == 1) {
         cardName = "ace";
       } else if (cardName == 11) {
+        rankCounter = 10;
         cardName = "jack";
       } else if (cardName == 12) {
+        rankCounter = 10;
         cardName = "queen";
       } else if (cardName == 13) {
+        rankCounter = 10;
         cardName = "king";
       }
 
-      // Create a new card with the current name, suit, and rank
+      // make a single card object variable
       var card = {
         name: cardName,
         suit: currentSuit,
         rank: rankCounter,
       };
 
-      // Add the new card to the deck
-      cardDeck.push(card);
+      // add the card to the deck
+      carddeck.push(card);
 
-      // Increment rankCounter to iterate over the next rank
-      rankCounter += 1;
+      counter = counter + 1;
     }
-
-    // Increment the suit index to iterate over the next suit
-    suitIndex += 1;
+    suitIndex = suitIndex + 1;
   }
 
-  shuffleCards(cardDeck);
-
-  // Return the completed card deck
-  return cardDeck;
+  return carddeck;
 };
 
-// Get a random index ranging from 0 (inclusive) to max (exclusive).
-var getRandomIndex = function () {
-  return Math.floor(Math.random() * 52);
+var getRandomIndex = function (size) {
+  return Math.floor(Math.random() * size);
 };
 
-//// Shuffle the elements in the cardDeck array
-var shuffleCards = function (cardDeck) {
-  // Loop over the card deck array once
-  var currentIndex = 0;
-  while (currentIndex < cardDeck.length) {
-    // Select a random index in the deck
-    var randomIndex = getRandomIndex(cardDeck.length);
-    // Select the card that corresponds to randomIndex
-    var randomCard = cardDeck[randomIndex];
-    // Select the card that corresponds to currentIndex
-    var currentCard = cardDeck[currentIndex];
-    // Swap positions of randomCard and currentCard in the deck
-    cardDeck[currentIndex] = randomCard;
-    cardDeck[randomIndex] = currentCard;
-    // Increment currentIndex
-    currentIndex = currentIndex + 1;
-  }
-  // Return the shuffled deck
-  return cardDeck;
-};
-
-var main = function () {
-  //make deck and shuffle
-  makeDeck();
-
-  //// make playerCards and computerCards (deal 2 cards each)
-  // var playerCard1 = cardDeck.pop();
-  // var computerCard1 = cardDeck.pop();
-  // var playerCard2 = cardDeck.pop();
-  // var computerCard2 = cardDeck.pop();
-
-  // playerCard.push(playerCard1);
-  // playerCard.push(playerCard2);
-  // computerCard.push(computerCard1);
-  // computerCard.push(computerCard2);
-
-  // console.log("playerCard");
-  // console.log(playerCard);
-  // console.log("computerCard");
-  // console.log(computerCard);
-
+var shuffleCards = function (cards) {
   var index = 0;
-  var playerTotalRank = 0;
-  var computerTotalRank = 0;
-  while (index <= playerCard.length) {
-    playerTotalRank = playerTotalRank + playerCard[index].rank;
-    console.log("Player Total Rank");
-    console.log(playerTotalRank);
-    if (playerTotalRank < 16) {
-      playerCard[index] = cardDeck.pop();
-    }
 
-    computerTotalRank = computerTotalRank + computerCard[index].rank;
-    console.log("Computer Total Rank");
-    console.log(computerTotalRank);
-    if (computerTotalRank < 16) {
-      computerCard[index] = cardDeck.pop();
-    }
+  while (index < cards.length) {
+    var randomIndex = getRandomIndex(cards.length);
+
+    var currentItem = cards[index];
+
+    var randomItem = cards[randomIndex];
+
+    cards[index] = randomItem;
+    cards[randomIndex] = currentItem;
 
     index = index + 1;
   }
+
+  return cards;
 };
 
-var game1 = function () {
-  if (playerCard1.rank + playerCard2 == 11) {
-    myOutputValue = `Player Wins`;
-  } else if (computerCard1.rank + computerCard2.rank == 11) {
-    myOutputValue = `Computer Wins`;
+// Initialise Card Deck
+var cardDeck = shuffleCards(makeDeck());
+
+// Draw 1 card to Hand Global function and Pop Deck
+var drawCard = function (NameHand) {
+  NameHand.push(cardDeck.pop());
+};
+
+////Count Hand of Player/Computer Global Function
+var CountHand = function (Player) {
+  var i = 0;
+  var sum = 0;
+  while (i < Player.length) {
+    var currentCard = Player[i];
+
+    //factoring Aces and player less than 2 cards, Ace = 11
+    if (currentCard.rank == 1 && Player.length < 3) {
+      currentCardRank = 11;
+      sum = sum + currentCardRank;
+    } else {
+      sum = sum + currentCard.rank;
+    }
+    i = i + 1;
+  }
+  return sum;
+};
+
+//define player and computer Hand in string
+
+var showHand = function (input) {
+  var index = 0;
+  var myOutputValue = "";
+
+  while (index < input.length) {
+    myOutputValue = ` ${input[index].name} of ${input[index].suit} <br> ${myOutputValue}`;
+    index = index + 1;
+  }
+
+  return myOutputValue;
+};
+
+// define dafault myOutputValue Player Functions
+var myOutputValuePlayer = function (input) {
+  return `Player: <br> ${showHand(input)}  <br> Score: ${CountHand(
+    input
+  )}. <br>`;
+};
+// define dafault myOutputValue computer Functions
+var myOutputValueComputer = function (input) {
+  return `Computer: <br> ${showHand(input)} <br> Score: ${CountHand(
+    input
+  )}. <br>`;
+};
+
+var main = function (input) {
+  // Initiate dealing at start game - deal card to player and PC each one-by-one
+  if (playerCard.length == 0) {
+    drawCard(playerCard);
+    drawCard(computerCard);
+    drawCard(playerCard);
+    drawCard(computerCard);
+
+    return `Click Start To Continue`;
+  }
+
+  // banker keep drawing till >16
+  var x = 0;
+  while (x < computerCard.length) {
+    if (CountHand(computerCard) < 16) {
+      drawCard(computerCard);
+    }
+    x = x + 1;
+  }
+
+  // if player 21 or computer 21 with 2 cards, else give player option to hit or stand
+  // whenver wins, restart deck and game.
+  if (CountHand(playerCard) == 21) {
+    playerWinCount = playerWinCount + 1;
+
+    GameOverOutputValue = `${myOutputValuePlayer(
+      playerCard
+    )} <br> ${myOutputValueComputer(
+      computerCard
+    )} <br> Player wins. <br> Player Wins: ${playerWinCount}. Computer Wins: ${computerWinCount}  `;
+    // restart game + new deck
+    playerCard = [];
+    computerCard = [];
+    cardDeck = shuffleCards(makeDeck());
+
+    return GameOverOutputValue;
+  } else if (CountHand(computerCard == 21)) {
+    computerWinCount = computerWinCount + 1;
+
+    GameOverOutputValue = `${myOutputValuePlayer(
+      playerCard
+    )} <br> ${myOutputValueComputer(
+      computerCard
+    )} <br> Computer wins. <br> Player Wins: ${playerWinCount}. Computer Wins: ${computerWinCount}`;
+    // restart game + new deck
+    playerCard = [];
+    computerCard = [];
+    cardDeck = shuffleCards(makeDeck());
+
+    return GameOverOutputValue;
+  } else if (CountHand(computerCard) > 21) {
+    playerWinCount = playerWinCount + 1;
+
+    GameOverOutputValue = `${myOutputValuePlayer(
+      playerCard
+    )} <br> ${myOutputValueComputer(
+      computerCard
+    )} <br> Player wins. <br> Player Wins: ${playerWinCount}. Computer Wins: ${computerWinCount}  `;
+    // restart game + new deck
+    playerCard = [];
+    computerCard = [];
+    cardDeck = shuffleCards(makeDeck());
+
+    return GameOverOutputValue;
+  } else if (CountHand(playerCard) < 21) {
+    return `${myOutputValuePlayer(playerCard)} <br>
+    Click "Hit" to draw 1 card or <br> "Stand" to tap.`;
+  }
+};
+
+// hit button
+var hit = function () {
+  drawCard(playerCard);
+
+  if (CountHand(playerCard) == 21) {
+    playerWinCount = playerWinCount + 1;
+
+    GameOverOutputValue = `${myOutputValuePlayer(
+      playerCard
+    )} <br> ${myOutputValueComputer(
+      computerCard
+    )} <br> Player wins. <br> Player Wins: ${playerWinCount}. Computer Wins: ${computerWinCount}  `;
+    // restart game + new deck
+    playerCard = [];
+    computerCard = [];
+    cardDeck = shuffleCards(makeDeck());
+
+    return GameOverOutputValue;
+  } else if (CountHand(playerCard) < 21) {
+    return `Player: <br> ${showHand(playerCard)}  <br> Score: ${CountHand(
+      playerCard
+    )}. <br>
+    Input "hit" to draw 1 card or <br> "stand" to tap.`;
+  } else if (CountHand(playerCard) > 21) {
+    computerWinCount = computerWinCount + 1;
+
+    GameOverOutputValue = `${myOutputValuePlayer(
+      playerCard
+    )} <br> ${myOutputValueComputer(
+      computerCard
+    )} <br> Computer wins. <br> Player Wins: ${playerWinCount}. Computer Wins: ${computerWinCount}`;
+
+    // restart game + new deck
+    playerCard = [];
+    computerCard = [];
+    cardDeck = shuffleCards(makeDeck());
+
+    return GameOverOutputValue;
+  }
+};
+
+// stand button
+var stand = function () {
+  if (CountHand(playerCard) > CountHand(computerCard)) {
+    playerWinCount = playerWinCount + 1;
+
+    GameOverOutputValue = `${myOutputValuePlayer(
+      playerCard
+    )} <br> ${myOutputValueComputer(
+      computerCard
+    )} <br> Player wins. <br> Player Wins: ${playerWinCount}. Computer Wins: ${computerWinCount}  `;
+    // restart game + new deck
+    playerCard = [];
+    computerCard = [];
+    cardDeck = shuffleCards(makeDeck());
+
+    return GameOverOutputValue;
+  } else if (CountHand(playerCard) < CountHand(computerCard)) {
+    computerWinCount = computerWinCount + 1;
+
+    GameOverOutputValue = `${myOutputValuePlayer(
+      playerCard
+    )} <br> ${myOutputValueComputer(
+      computerCard
+    )} <br> Computer wins. <br> Player Wins: ${playerWinCount}. Computer Wins: ${computerWinCount}`;
+
+    // restart game + new deck
+    playerCard = [];
+    computerCard = [];
+    cardDeck = shuffleCards(makeDeck());
+
+    return GameOverOutputValue;
+  } else if (CountHand(playerCard) == CountHand(computerCard)) {
+    GameOverOutputValue = `${myOutputValuePlayer(
+      playerCard
+    )} <br> ${myOutputValueComputer(
+      computerCard
+    )} <br> It's a draw. <br> Player Wins: ${playerWinCount}. Computer Wins: ${computerWinCount}`;
+
+    // restart game + new deck
+    playerCard = [];
+    computerCard = [];
+    cardDeck = shuffleCards(makeDeck());
+
+    return GameOverOutputValue;
   }
 };
